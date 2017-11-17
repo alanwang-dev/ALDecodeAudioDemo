5167491f18689334eb7623b6c997af4294ea6f4c
alanwang-dev <alanwang.dev@gmail.com>
2017-11-17 15:35:07 +0800

    Initial Commit

---
+++ ALDecodeAudio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALDecodeAudio
+//
+//  Created by iVermisseDich on 2017/11/17.
+//  Copyright © 2017年 com.ksyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ALDecodeAudio/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ALDecodeAudio
+//
+//  Created by iVermisseDich on 2017/11/17.
+//  Copyright © 2017年 com.ksyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
